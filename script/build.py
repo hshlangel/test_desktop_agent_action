829b37ee40bbd95c80ef19dd5cce4a2e7c3017fa
shlangelhu <146046294+shlangelhu@users.noreply.github.com>
2023-11-10 11:00:41 +0800

    action mac test
    action mac test

--- script/build.py
+++ script/build.py
@@ -126,7 +126,7 @@ def main(file : str):
         cmd = f'pyinstaller -i ./{app_project_name}/app/resource/favicon.ico -n {exe_name} ./{app_project_name}/app/main.py --onefile --noconsole --uac-admin --upx-dir=./upx-4.2.1-win64 --version-file=./{app_project_name}/app/version.txt'
     else:
         #cmd = f'pyinstaller -F -w -i ./{app_project_name}/app/resource/favicon.icns -n {exe_name} ./{app_project_name}/app/main.py'
-        cmd = f'pyinstaller -F -w -i ./{app_project_name}/app/resource/favicon.icns -n {exe_name} ./{app_project_name}/app/test.py --add-data ./{app_project_name}/app/agentapp_decrypt.db:.'
+        cmd = f'pyinstaller -F -w -i ./{app_project_name}/app/resource/favicon.icns -n {exe_name} ./{app_project_name}/app/test.py --add-data {decrypt_dbname}:.'
     os.system(cmd)
 
     print('pack app success')
